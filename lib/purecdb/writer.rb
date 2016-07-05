cfc506b253798481f9d7f0572fcf0077c00e5380
Vidar Hokstad <vidar@hokstad.com>
2016-07-05 04:43:20 +0100

    Documentation improvement, + turned hash_fill_actor into attribute

--- lib/purecdb/writer.rb
+++ lib/purecdb/writer.rb
@@ -1,14 +1,57 @@
 module PureCDB
+  #
+  # Write 32 or 64 bit CDB files
+  #
+  # == Memory considerations
+  #
+  # While the entry is written to the target object immediately on calling #store, the actual
+  # hash tables can not be written until the full dataset is ready. You must therefore be able
+  # to hold the hash of each key (including duplicates) and the position in the file the full
+  # netry is stored at in memory while building the CDB file.
+  #
+  # It would be possible to write this to a temporary file at the cost of performance, but the
+  # current implementation does not do this.
+  #
+  # As a compromise, the current implementation stores the hashes and positions as a BER encoded
+  # string per hash bucket until it is ready to write it to disk.
+  #
   class Writer < Base
-    # This just needs to be <= 1. The lower it is, the fewer records will collide. The closer to 1 it is,
-    # the more frequently the reader may have to engage in potentially lengthy (worst case scanning all the
-    # records) probing to find the right entry
-    def hash_fill_factor
-      0.7
-    end
+    # How full any given hash table is allowed to get, as a float between 0 and 1.
+    #
+    # Needs to be <= 1. The lower it is, the fewer records will collide. The closer to 1 it is,
+    # the more frequently the reader may have to engage in potentially lengthy (worst case
+    # scanning all the records) probing to find the right entry
+    attr_accessor :hash_fill_factor
 
+    # Open a CDB file for writing, or preparing an IO like object for writing.
+    #
+    # :call-seq:
+    #   w = PureCDB::Writer.new(target)
+    #   w = PureCDB::Writer.new(target, *options)
+    #   PureCDB::Writer.new(target)  {|w| ... }
+    #   PureCDB::Writer.new(target, *options) {|w| ... }
+    #
+    # If +:mode+ is passed in +options+, it must be the integers 32 or 64, indicating whether
+    # you wish to write a standard (32 bit) CDB file, or a 64 bit CDB-like file. The default
+    # is 32.
+    #
+    # If +target+ is a +String+ it is treated as a filename of a file to be opened to write to.
+    # Otherwise +target+ is assumed to be an IO-like object that ideally responds to #sysseek
+    # and #syswrite. If it doesn't, it will be wrapped with an object delegating #sysseek and
+    # #syswrite to #seek and #write respectively, and these must be present.
+    #
+    # (+IO+ and +StringIO+ both satisfy these requirements)
+    #
+    # If passed a block, the writer is yielded to the block and PureCDB::Writer#close is called
+    # afterwards.
+    #
+    # **WARNING:** To complete writing the hash tables, you *must* ensure #close is called
+    # when you are done.
+    #
     def initialize target, *options
       super *options
+
+      @hash_fill_factor = 0.7
 
       set_mode(32) if @mode == :detect
 
@@ -34,29 +77,38 @@ module PureCDB
       end
     end
 
+    # Write out the hashes and hash pointers, and close the target if it responds to #close
+    #
     def close
       write_hashes
       write_hashptrs
       @io.close if @io.respond_to?(:close)
     end
-   
-   def store key,value
-     # In an attempt to save memory, we pack the hash data we gather into
-     # strings of BER compressed integers...     
-     h = hash(key)
-     hi = (h % num_hashes)
-     @hashes[hi] ||= ""
-     
-     header = build_header(key.length, value.length)
-     @io.syswrite(header+key+value)
-     size = header.size + key.size + value.size
-     @hashes[hi] += [h,@pos].pack("ww") # BER compressed     
-     @pos += size
-   end
 
-   def self.open target, *options, &block
-     Writer.new(target, *options, &block)
-   end
+    # Store 'value' under 'key'.
+    #
+    # Multiple values can we stored for the same key by calling #store multiple times
+    # with the same key value.
+    def store key,value
+      # In an attempt to save memory, we pack the hash data we gather into
+      # strings of BER compressed integers...
+      h = hash(key)
+      hi = (h % num_hashes)
+      @hashes[hi] ||= ""
+
+      header = build_header(key.length, value.length)
+      @io.syswrite(header+key+value)
+      size = header.size + key.size + value.size
+      @hashes[hi] += [h,@pos].pack("ww") # BER compressed
+      @pos += size
+    end
+
+    #
+    # Alternative to PureCDB::Writer.new(target,options) ..
+    #
+    def self.open target, *options, &block
+      Writer.new(target, *options, &block)
+    end
    
    private
     def write_hashes
